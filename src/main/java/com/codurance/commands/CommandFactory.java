4dd27d37b7eee4fd1edba230e02c96a650349eb8
Tom <thomas.spencer@codurance.com>
2020-09-19 12:34:39 +0100

    Edit unused fields

--- src/main/java/com/codurance/commands/CommandFactory.java
+++ src/main/java/com/codurance/commands/CommandFactory.java
@@ -1,12 +1,9 @@
 package com.codurance.commands;
 
-import com.codurance.Rover;
-import java.util.HashMap;
 import java.util.Map;
 
 public class CommandFactory {
 
-  private Rover rover;
   private static final String MOVE = "M";
   private static final String LEFT = "L";
   private static final String RIGHT = "R";
