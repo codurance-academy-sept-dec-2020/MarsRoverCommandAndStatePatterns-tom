ffec8ff578c83d96660722b1f7a64e42566b2458
Tom <thomas.spencer@codurance.com>
2020-09-19 21:01:54 +0100

    Inline Coordinate calculations

--- src/main/java/com/codurance/Grid.java
+++ src/main/java/com/codurance/Grid.java
@@ -7,26 +7,18 @@ public class Grid {
   private static final int MAX_HEIGHT = 10;
 
   public Coordinate nextEast(Coordinate coordinate){
-    int x = coordinate.x;
-    x = (x + 1) % MAX_WIDTH;
-    return new Coordinate(x, coordinate.y);
+    return new Coordinate((coordinate.x + 1) % MAX_WIDTH, coordinate.y);
   }
 
   public Coordinate nextNorth(Coordinate coordinate){
-    int y = coordinate.y;
-    y = (y + 1) % MAX_HEIGHT;
-    return new Coordinate(coordinate.x, y);
+    return new Coordinate(coordinate.x, (coordinate.y + 1) % MAX_HEIGHT);
   }
 
   public Coordinate nextSouth(Coordinate coordinate){
-    int y = coordinate.y;
-    y = (y > 0) ? y - 1 : MAX_HEIGHT - 1;
-    return new Coordinate(coordinate.x, y);
+    return new Coordinate(coordinate.x, (coordinate.y > 0) ? coordinate.y - 1 : MAX_HEIGHT - 1);
   }
 
   public Coordinate nextWest(Coordinate coordinate){
-    int x = coordinate.x;
-    x = (x > 0) ? x - 1 : MAX_WIDTH - 1;
-    return new Coordinate(x, coordinate.y);
+    return new Coordinate((coordinate.x > 0) ? coordinate.x - 1 : MAX_WIDTH - 1, coordinate.y);
   }
 }
